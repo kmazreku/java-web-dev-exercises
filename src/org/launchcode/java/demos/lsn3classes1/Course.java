8b72ee2e8ca0b3936648b2f0f9489b5244170daa
Kalinka Georgieva <timbarrrli@iMac.local>
2020-06-24 12:18:56 -0400

    Lesson 3 Ex

---
+++ src/org/launchcode/java/demos/lsn3classes1/Course.java
@@ -0,0 +1,14 @@
+package org.launchcode.java.demos.lsn3classes1;
+
+import java.util.ArrayList;
+
+
+public class Course {
+    public static void main(String[] args) {
+        int numberOfStudents = 0;
+       ArrayList<Student> enrolledStudents = new ArrayList<>();
+       String courseName;
+       enrolledStudents.add(new Student("Kalinka", 1,1,4.0));
+
+    }
+}
--- src/org/launchcode/java/demos/lsn3classes1/Student.java
+++ src/org/launchcode/java/demos/lsn3classes1/Student.java
@@ -10,4 +10,59 @@ public class Student {
     private int numberOfCredits = 0;
     private double gpa = 0.0;
 
+    public Student(String name, int studentId,
+                   int numberOfCredits, double gpa) {
+        this.name = name;
+        this.studentId = studentId;
+        this.numberOfCredits = numberOfCredits;
+        this.gpa = gpa;
+    }
+
+    public Student(String name, int studentId) {
+        this(name, studentId, 0, 0);
+    }
+
+    /* getters and setters omitted */
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String aName) {
+        name = aName;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int aStudentId) {
+        studentId = aStudentId;
+    }
+    public int getNumberOfCredits() {
+        return numberOfCredits;
+    }
+
+    public void setNumberOfCredits(int aNumberOfCredits) {
+        numberOfCredits = aNumberOfCredits;
+    }
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double aGpa) {
+        gpa = aGpa;
+    }
+
+//    public Student(String name) {
+//        this(name, nextStudentId);
+//        nextStudentId++;
+//    }
+//
+//    public String studentInfo() {
+//        return (this.name + " has a GPA of: " + this.gpa);
+//    }
+
+    /* getters and setters omitted */
+
 }
+
