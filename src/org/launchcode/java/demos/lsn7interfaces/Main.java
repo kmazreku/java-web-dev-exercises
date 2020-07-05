83af31cb67e5a8270f8d218ebbe84ccbef0d57f3
Kalinka Georgieva <timbarrrli@iMac.local>
2020-07-05 10:43:51 -0400

    Interfaces Ex

--- src/org/launchcode/java/demos/lsn7interfaces/Main.java
+++ src/org/launchcode/java/demos/lsn7interfaces/Main.java
@@ -1,6 +1,7 @@
 package org.launchcode.java.demos.lsn7interfaces;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args){
@@ -10,10 +11,27 @@ public class Main {
 
         // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name'
         //  field.
+        Comparator comparator = new FlavorComparator();
+        flavors.sort(comparator);
+
+
+
 
         // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
         //  field.
+        Comparator comparatorCone = new ConeComparator();
+        cones.sort(comparatorCone);
 
         // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+
+        for(Flavor item: flavors){
+            System.out.println(item.toString());
+        }
+
+
+        for (Cone item: cones){
+            System.out.println(item.toString());
+        }
+
     }
 }
